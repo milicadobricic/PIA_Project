faae08595c7ba49df1d175280a6579d6ba64ab04
Milica Dobričić <micikadobricic96@gmail.com>
2021-02-18 23:48:19 +0100

    Backend initialized

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,20 @@
+import bodyParser from 'body-parser';
+import cors from 'cors';
 import express from 'express';
+import mongoose from 'mongoose';
+
 const app = express();
-app.get('/', (req, res) => res.send('Hello World!'));
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost:27017/pia_project').then(() => console.log('mongoose connected'));
+
+const router = express.Router();
+
+router.route('/login').get((req, res) => {
+    res.json('Hello, World!');
+});
+
+app.use('/', router);
 app.listen(4000, () => console.log(`Express server running on port 4000`));
